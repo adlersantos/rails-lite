f95f91d424f6dc24a8e575d05dbea59fb5925ccc
Ned Ruggeri <ruggeri@self-loop.com>
2013-06-07 10:03:58 -0700

    rewrite params server to be simpler

--- test/02_params_server.rb
+++ test/02_params_server.rb
@@ -10,27 +10,14 @@ require 'rails_lite'
 server = WEBrick::HTTPServer.new :Port => 8080
 trap('INT') { server.shutdown }
 
-class StatusesController < ControllerBase
-  def create
-    render_content(params.to_json, "text/json")
-  end
-
-  def new
-  end
-
-  def show
-    render_content("status ##{params[:id]}", "text/text")
+class ExampleController < ControllerBase
+  def go
+    render_content("params", "text/json")
   end
 end
 
 server.mount_proc '/' do |req, res|
-  router = Router.new
-  router.draw do
-    post Regexp.new("^/statuses$"), StatusesController, :create
-    get Regexp.new("^/statuses/new$"), StatusesController, :new
-  end
-
-  route = router.run(req, res)
+  contr = ExampleController.new(req, res).go
 end
 
 server.start
