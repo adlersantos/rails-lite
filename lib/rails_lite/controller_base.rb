bccc977155ba9f319e3e8b13c143e907c3062922
Adler Santos <adlersantos712@gmail.com>
2013-08-12 14:56:42 -0700

    working query_string

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -1,26 +1,48 @@
 require 'erb'
+require 'active_support/core_ext'
 require_relative 'params'
 require_relative 'session'
 
 class ControllerBase
   attr_reader :params
 
-  def initialize(req, res, route_params)
+  def initialize(req, res, route_params={})
+    @request, @response = req, res
+    @params = Params.new(req, route_params)
   end
 
   def session
+    @session ||= Session.new(@request)
   end
 
   def already_rendered?
+    !!@already_rendered
   end
 
   def redirect_to(url)
+    unless @response_built
+      @response.status = "302"
+      @response.header["location"] = url
+
+      session.store_session(@response)
+      @response_built = true
+    end
   end
 
   def render_content(content, type)
+    unless already_rendered?
+      @response.content_type = type
+      @response.body = content
+
+      session.store_session(@response)
+      @already_rendered = true
+    end
   end
 
   def render(template_name)
+    file = File.read("views/#{self.class.to_s.underscore}/#{template_name}.html.erb")
+    eval_contents = eval("ERB.new(file).result(binding)")
+    render_content(eval_contents, 'text/html')
   end
 
   def invoke_action(name)
--- lib/rails_lite/session.rb
+++ lib/rails_lite/session.rb
@@ -3,14 +3,24 @@ require 'webrick'
 
 class Session
   def initialize(req)
+    req.cookies.each do |cookie|
+      if cookie.name == '_rails_lite_app'
+        @cookie = JSON.parse(cookie.value)
+        return
+      end
+    end
+    @cookie = {}
   end
 
   def [](key)
+    @cookie[key]
   end
 
   def []=(key, val)
+    @cookie[key] = val
   end
 
   def store_session(res)
+    res.cookies << WEBrick::Cookie.new('_rails_lite_app', @cookie.to_json)
   end
 end
